f461c9d53ca09de9f9375ee25bc4158bd7f8d16e
Beverly <duran.beverly@gmail.com>
2023-05-05 15:56:37 -0400

    sign up action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -40,6 +40,26 @@ export const restoreUser = () => async (dispatch) => {
     return response;
 };
 
+// frontend/src/store/session.js
+// ...
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+// ...
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
