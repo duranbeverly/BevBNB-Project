e6f041d1869f7efc1d395d6deee3f4b3e99310fd
Beverly <duran.beverly@gmail.com>
2023-05-08 00:10:12 -0400

    seeded updates

--- backend/db/seeders/20230416163607-reviews.js
+++ backend/db/seeders/20230416163607-reviews.js
@@ -49,6 +49,186 @@ module.exports = {
                 userId: 2,
                 review: 'The host was awful.This guy clearly only cares about money',
                 stars: 1
+            },
+            {
+                spotId: 4,
+                userId: 3,
+                review: 'What a beautiful location! We loved the secluded feel and the ocean views. Highly recommend!',
+                stars: 5
+            },
+            {
+                spotId: 4,
+                userId: 2,
+                review: 'The treehouse was amazing! It was clean, cozy, and had all the amenities we needed. We will definitely be back.',
+                stars: 4
+            },
+            {
+                spotId: 4,
+                userId: 1,
+                review: 'The treehouse was beautiful but unfortunately we had some issues with the plumbing. The host was responsive and did their best to help us though.',
+                stars: 3
+            },
+            {
+                spotId: 5,
+                userId: 4,
+                review: 'This place was incredible! The views were amazing and the cabin was super cozy. Highly recommend.',
+                stars: 5
+            },
+            {
+                spotId: 5,
+                userId: 3,
+                review: 'The cabin was beautiful and clean, but the price was a bit steep for us.',
+                stars: 3
+            },
+            {
+                spotId: 5,
+                userId: 2,
+                review: 'The host was great and the cabin was beautiful! We loved the fireplace and the hot tub. Would definitely recommend.',
+                stars: 4
+            },
+            {
+                spotId: 6,
+                userId: 1,
+                review: 'The earth home was really unique and fun to stay in! The host was also super friendly and accommodating.',
+                stars: 4
+            },
+            {
+                spotId: 6,
+                userId: 2,
+                review: 'We loved our stay in the earth home! It was cozy and had all the amenities we needed. Would definitely recommend to others.',
+                stars: 5
+            },
+            {
+                spotId: 6,
+                userId: 3,
+                review: 'The earth home was great but we had some issues with the heating. The host was helpful and did their best to fix the problem though.',
+                stars: 3
+            },
+            {
+                spotId: 7,
+                userId: 4,
+                review: 'The tiny home was so cute and cozy! We loved the off-the-grid feel and the views were incredible.',
+                stars: 5
+            },
+            {
+                spotId: 7,
+                userId: 2,
+                review: 'The tiny home was great but we had some issues with the electricity. The host was responsive and did their best to help us though.',
+                stars: 3
+            },
+            {
+                spotId: 7,
+                userId: 1,
+                review: 'The host was really friendly and helpful, and the tiny home was adorable! Would definitely recommend to others.',
+                stars: 4
+            },
+            {
+                spotId: 8,
+                userId: 3,
+                review: 'The beach house was beautiful and had amazing views. The host was also really friendly and accommodating.',
+                stars: 4
+            },
+            {
+                spotId: 8,
+                userId: 2,
+                review: 'We loved our stay in the beach house! It was clean, spacious, and had all the amenities we needed. Would definitely recommend.',
+                stars: 5
+            },
+            {
+                spotId: 8,
+                userId: 1,
+                review: 'The beach house was nice but unfortunately we had some issues with the noise from the nearby construction. The host did their best to help us though.',
+                stars: 3
+            },
+            {
+                spotId: 9,
+                userId: 2,
+                review: 'This place is a hidden gem, surrounded by beautiful forests and nature. The house itself is cozy and well-equipped with everything you need. Highly recommend!',
+                stars: 5
+            },
+            {
+                spotId: 9,
+                userId: 3,
+                review: 'We had a great time at this cabin in the woods. The owner was very responsive and accommodating, and the location was perfect for hiking and exploring the nearby parks.',
+                stars: 4
+            },
+            {
+                spotId: 10,
+                userId: 1,
+                review: 'This beach house was amazing! The view was breathtaking and the house itself was well-decorated and comfortable. The owner was also very friendly and helpful.',
+                stars: 5
+            },
+            {
+                spotId: 10,
+                userId: 3,
+                review: 'The location of this beach house was unbeatable, right on the ocean. However, the house itself was a bit outdated and could use some upgrades.',
+                stars: 3
+            },
+            {
+                spotId: 11,
+                userId: 1,
+                review: 'This treehouse was such a unique and special experience. We loved being up in the trees and feeling like we were in our own little world. The owner was also very friendly and accommodating.',
+                stars: 5
+            },
+            {
+                spotId: 11,
+                userId: 4,
+                review: 'The treehouse was definitely a cool experience, but it was a bit smaller than we expected. The location was also pretty remote and difficult to get to.',
+                stars: 3
+            },
+            {
+                spotId: 12,
+                userId: 2,
+                review: 'This cabin was the perfect getaway for a peaceful and relaxing vacation. The owner was very responsive and the location was great for exploring the nearby parks and trails.',
+                stars: 4
+            },
+            {
+                spotId: 12,
+                userId: 3,
+                review: 'We had a great time at this cabin, but there were a few minor issues with cleanliness and maintenance. Overall, though, it was a lovely spot and we would recommend it to others.',
+                stars: 3
+            },
+            {
+                spotId: 13,
+                userId: 1,
+                review: 'This off-the-grid tiny home was a really cool experience. We loved being able to disconnect and get away from the world for a bit. The owner was also very friendly and accommodating.',
+                stars: 4
+            },
+            {
+                spotId: 13,
+                userId: 2,
+                review: 'We had a bit of trouble with some of the amenities at this tiny home, but the owner was very responsive and did their best to fix the issues. Overall, it was a fun and unique experience.',
+                stars: 3
+            },
+            {
+                spotId: 14,
+                userId: 4,
+                review: 'While the earth home was definitely unique, it was also pretty rustic and not as comfortable as we would have liked. The location was also a bit remote and hard to get to.',
+                stars: 2
+            },
+            {
+                spotId: 15,
+                userId: 2,
+                review: 'Loved the earth home. Great design and really unique experience. Would definitely recommend!',
+                stars: 5
+            },
+            {
+                spotId: 15,
+                userId: 3,
+                review: 'The location was perfect and the earth home was lovely. The only downside was that the hot tub was not working during our stay.',
+                stars: 4
+            },
+            {
+                spotId: 15,
+                userId: 4,
+                review: 'The earth home was really interesting and fun to stay in. Loved the views and the surrounding nature.',
+                stars: 4
+            },
+            {
+                spotId: 16,
+                userId: 3,
+                review: 'The off the grid tiny home was a great experience. Loved being so close to nature and the unique design of the home.',
+                stars: 5
             }
 
         ], {})
--- backend/db/seeders/20230416163800-spot-images.js
+++ backend/db/seeders/20230416163800-spot-images.js
@@ -26,15 +26,460 @@ module.exports = {
                 preview: true
             },
             {
+                spotId: 1,
+                url: 'https://unsplash.com/photos/14GPEHuhl38',
+                preview: false
+            },
+            {
+                spotId: 1,
+                url: 'https://unsplash.com/photos/O3zdTFCnHcg',
+                preview: false
+            },
+            {
+                spotId: 1,
+                url: 'https://unsplash.com/photos/Us2KDzmnUCo',
+                preview: false
+            },
+            {
+                spotId: 1,
+                url: 'https://unsplash.com/photos/7EqQ1s3wIAI',
+                preview: false
+            },
+            {
+                spotId: 1,
+                url: 'https://unsplash.com/photos/MDteiLH1CZY',
+                preview: false
+            },
+            {
                 spotId: 2,
                 url: 'https://unsplash.com/photos/y3_AHHrxUBY',
                 preview: true
+            },
+            {
+                spotId: 2,
+                url: 'https://unsplash.com/photos/waAAaeC9hns',
+                preview: false
+            },
+            {
+                spotId: 2,
+                url: 'https://unsplash.com/photos/h8wLc3lbDuA',
+                preview: false
+            },
+            {
+                spotId: 2,
+                url: 'https://unsplash.com/photos/h0AnGGgseio',
+                preview: false
+            },
+            {
+                spotId: 2,
+                url: 'https://unsplash.com/photos/oUdt2BJrLJE',
+                preview: false
+            },
+            {
+                spotId: 2,
+                url: 'https://unsplash.com/photos/64jW2tDpuI8',
+                preview: false
             },
             {
                 spotId: 3,
                 url: 'https://unsplash.com/photos/WJDR8_QxVR8',
                 preview: true
             },
+            {
+                spotId: 3,
+                url: 'https://unsplash.com/photos/nWX4pKwzLoE',
+                preview: false
+            },
+            {
+                spotId: 3,
+                url: 'https://unsplash.com/photos/QHXNWzbghH4',
+                preview: false
+            },
+            {
+                spotId: 3,
+                url: 'https://unsplash.com/photos/DH_u2aV3nGM',
+                preview: false
+            },
+            {
+                spotId: 3,
+                url: 'https://unsplash.com/photos/KMn4VEeEPR8',
+                preview: false
+            },
+            {
+                spotId: 3,
+                url: 'https://unsplash.com/photos/-u-30ap3rEw',
+                preview: false
+            },
+            {
+                spotId: 4,
+                url: 'https://unsplash.com/photos/EpeNGhitrlc',
+                preview: true
+            },
+            {
+                spotId: 4,
+                url: 'https://unsplash.com/photos/69C177OYn7c',
+                preview: false
+            },
+            {
+                spotId: 4,
+                url: 'https://unsplash.com/photos/nXOB-wh4Oyc',
+                preview: false
+            },
+            {
+                spotId: 4,
+                url: 'https://unsplash.com/photos/nis5gvoboyI',
+                preview: false
+            },
+            {
+                spotId: 4,
+                url: 'https://unsplash.com/photos/2qLT_Rq-2tk',
+                preview: false
+            },
+            {
+                spotId: 4,
+                url: 'https://unsplash.com/photos/1za4NHi9zjI',
+                preview: false
+            },
+            {
+                spotId: 5,
+                url: 'https://unsplash.com/photos/GEv0x00erqU',
+                preview: true
+            },
+            {
+                spotId: 5,
+                url: 'https://unsplash.com/photos/JpWtmyjMI30',
+                preview: false
+            },
+            {
+                spotId: 5,
+                url: 'https://unsplash.com/photos/ZkDTz_rAgKw',
+                preview: false
+            },
+            {
+                spotId: 5,
+                url: 'https://unsplash.com/photos/_u8KhAZRGHs',
+                preview: false
+            },
+            {
+                spotId: 5,
+                url: 'https://unsplash.com/photos/cjhuXRtRT0Y',
+                preview: false
+            },
+            {
+                spotId: 5,
+                url: 'https://unsplash.com/photos/OD9EOzfSOh0',
+                preview: false
+            },
+            {
+                spotId: 6,
+                url: 'https://unsplash.com/photos/nfF5-G6cFwY',
+                preview: true
+            },
+            {
+                spotId: 6,
+                url: 'https://unsplash.com/photos/Mn5vLHPLTuw',
+                preview: false
+            },
+            {
+                spotId: 6,
+                url: 'https://unsplash.com/photos/Y3Hbh7wB8CI',
+                preview: false
+            },
+            {
+                spotId: 6,
+                url: 'https://unsplash.com/photos/-T8tYN-3cMg',
+                preview: false
+            },
+            {
+                spotId: 6,
+                url: 'https://unsplash.com/photos/OlkXc9xlDks',
+                preview: false
+            },
+            {
+                spotId: 6,
+                url: 'https://unsplash.com/photos/MrDfuenOS9s',
+                preview: false
+            },
+            {
+                spotId: 7,
+                url: 'https://unsplash.com/photos/YG2MysGbT_M',
+                preview: true
+            },
+            {
+                spotId: 7,
+                url: 'https://unsplash.com/photos/iANAdaNu7mg',
+                preview: false
+            },
+            {
+                spotId: 7,
+                url: 'https://unsplash.com/photos/mw_mj-noYHM',
+                preview: false
+            },
+            {
+                spotId: 7,
+                url: 'https://unsplash.com/photos/O7WjrXiKy_s',
+                preview: false
+            },
+            {
+                spotId: 7,
+                url: 'https://unsplash.com/photos/JhYbY9nv0rU',
+                preview: false
+            },
+            {
+                spotId: 7,
+                url: 'https://unsplash.com/photos/3vv1A8zNG9A',
+                preview: false
+            },
+            {
+                spotId: 8,
+                url: 'https://unsplash.com/photos/Mhiuw5Rp1WU',
+                preview: true
+            },
+            {
+                spotId: 8,
+                url: 'https://unsplash.com/photos/ao1ZaY55bvs',
+                preview: false
+            },
+            {
+                spotId: 8,
+                url: 'https://unsplash.com/photos/MTSILZYVcdw',
+                preview: false
+            },
+            {
+                spotId: 8,
+                url: 'https://unsplash.com/photos/XtnNrQYC7ts',
+                preview: false
+            },
+            {
+                spotId: 8,
+                url: 'https://unsplash.com/photos/HmieZw0YOC0',
+                preview: false
+            },
+            {
+                spotId: 8,
+                url: 'https://unsplash.com/photos/dS1EQ30SQcU',
+                preview: false
+            },
+            {
+                spotId: 9,
+                url: 'https://unsplash.com/photos/j8J-9D4i9EA',
+                preview: true
+            },
+            {
+                spotId: 9,
+                url: 'https://unsplash.com/photos/05A3CzImkhw',
+                preview: false
+            },
+            {
+                spotId: 9,
+                url: 'https://unsplash.com/photos/kSslUrbE2Kc',
+                preview: false
+            },
+            {
+                spotId: 9,
+                url: 'https://unsplash.com/photos/2jlRllahuMk',
+                preview: false
+            },
+            {
+                spotId: 9,
+                url: 'https://unsplash.com/photos/ATprPSzYu00',
+                preview: false
+            },
+            {
+                spotId: 9,
+                url: 'https://unsplash.com/photos/56WKMCORs-0',
+                preview: false
+            },
+            {
+                spotId: 10,
+                url: 'https://unsplash.com/photos/OgcJIKRnRC8',
+                preview: true
+            },
+            {
+                spotId: 10,
+                url: 'https://unsplash.com/photos/EbivdbB83Y0',
+                preview: false
+            },
+            {
+                spotId: 10,
+                url: 'https://unsplash.com/photos/51adhgg5KkE',
+                preview: false
+            },
+            {
+                spotId: 10,
+                url: 'https://unsplash.com/photos/duo-xV0TU7s',
+                preview: false
+            },
+            {
+                spotId: 10,
+                url: 'https://unsplash.com/photos/IWenq-4JHqo',
+                preview: false
+            },
+            {
+                spotId: 10,
+                url: 'https://unsplash.com/photos/DEnuB_zok9M',
+                preview: false
+            },
+            {
+                spotId: 11,
+                url: 'https://unsplash.com/photos/AQjkC4sax6s',
+                preview: true
+            },
+            {
+                spotId: 11,
+                url: 'https://unsplash.com/photos/PyFzygP2eNg',
+                preview: false
+            },
+            {
+                spotId: 11,
+                url: 'https://unsplash.com/photos/-BqB2zptPQo',
+                preview: false
+            },
+            {
+                spotId: 11,
+                url: 'https://unsplash.com/photos/Y41w7pyxE18',
+                preview: false
+            },
+            {
+                spotId: 11,
+                url: 'https://unsplash.com/photos/ZWGexQLecAI',
+                preview: false
+            },
+            {
+                spotId: 11,
+                url: 'https://unsplash.com/photos/58uZCE8zrdk',
+                preview: false
+            },
+            {
+                spotId: 12,
+                url: 'https://unsplash.com/photos/MTSILZYVcdw',
+                preview: true
+            },
+            {
+                spotId: 12,
+                url: 'https://unsplash.com/photos/XtnNrQYC7ts',
+                preview: false
+            },
+            {
+                spotId: 12,
+                url: 'https://unsplash.com/photos/HmieZw0YOC0',
+                preview: false
+            },
+            {
+                spotId: 12,
+                url: 'https://unsplash.com/photos/dS1EQ30SQcU',
+                preview: false
+            },
+            {
+                spotId: 12,
+                url: 'https://unsplash.com/photos/Mhiuw5Rp1WU',
+                preview: false
+            },
+            {
+                spotId: 12,
+                url: 'https://unsplash.com/photos/ao1ZaY55bvs',
+                preview: false
+            },
+            {
+                spotId: 13,
+                url: 'https://unsplash.com/photos/dyj7RTs85Fs',
+                preview: true
+            },
+            {
+                spotId: 13,
+                url: 'https://unsplash.com/photos/nwE2CE1ksEE',
+                preview: false
+            },
+            {
+                spotId: 13,
+                url: 'https://unsplash.com/photos/TlROlpRnRrw',
+                preview: false
+            },
+            {
+                spotId: 13,
+                url: 'https://unsplash.com/photos/388bwjYQ1Kk',
+                preview: false
+            },
+            {
+                spotId: 13,
+                url: 'https://unsplash.com/photos/VjpuXU-4UE4',
+                preview: false
+            },
+            {
+                spotId: 13,
+                url: 'https://unsplash.com/photos/z_lpmqd2t_c',
+                preview: false
+            },
+            {
+                spotId: 14,
+                url: 'https://unsplash.com/photos/iYVmcIb94gs',
+                preview: true
+            },
+            {
+                spotId: 14,
+                url: 'https://unsplash.com/photos/P0B9b0mY4W4',
+                preview: false
+            },
+            {
+                spotId: 14,
+                url: 'https://unsplash.com/photos/HEq2Umht4rE',
+                preview: false
+            },
+            {
+                spotId: 14,
+                url: 'https://unsplash.com/photos/Bkci_8qcdvQ',
+                preview: false
+            },
+            {
+                spotId: 14,
+                url: 'https://unsplash.com/photos/avJ9uz9Qhcw',
+                preview: false
+            },
+            {
+                spotId: 14,
+                url: 'https://unsplash.com/photos/bQTVoJHrkO0',
+                preview: false
+            },
+            {
+                spotId: 15,
+                url: 'https://unsplash.com/photos/CppuxXh6nxE',
+                preview: true
+            },
+            {
+                spotId: 15,
+                url: 'https://unsplash.com/photos/IA_BATrHzXo',
+                preview: false
+            },
+            {
+                spotId: 15,
+                url: 'https://unsplash.com/photos/E3QBpdLAv0k',
+                preview: false
+            },
+            {
+                spotId: 15,
+                url: 'https://unsplash.com/photos/9yoPEVoSTcA',
+                preview: false
+            },
+            {
+                spotId: 15,
+                url: 'https://unsplash.com/photos/QVl0nFEp1Dk',
+                preview: false
+            },
+            {
+                spotId: 16,
+                url: 'https://unsplash.com/photos/OtXADkUh3-I',
+                preview: true
+            },
+            {
+                spotId: 16,
+                url: 'https://unsplash.com/photos/I4MhWjuf5Xk',
+                preview: false
+            },
+            {
+                spotId: 16,
+                url: 'https://unsplash.com/photos/sf2ICUgMky0',
+                preview: false
+            }
         ], {})
     },
 
